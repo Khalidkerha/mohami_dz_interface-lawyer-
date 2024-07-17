6eb4f7274a0cb7ebb12fe5cdaeadf4d6053238d8
khalidkerha <k_kerha@estin.dz>
2024-07-17 17:47:04 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Header from './header';
-import './App.css';
-import Location from './LocationInfo';
-import axios from 'axios';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutApp from './user_profile/About/about_app';
+import HomeApp from './user_profile/home page/home_page_app';
+import LawyerDirectoryApp from "./user_profile/lawyery directory/lawyerderectory_app";
+import Result from "./user_profile/resutl_page/result_app";
+import DeatailApp from "./user_profile/deatil info/detail_app";
+import ReviewApp from "./user_profile/review lawyer/review_app";
 function App() {
-  const [ipAddress, setIpAddress] = useState('');
-
-  useEffect(() => {
-    // Fetch the user's IP address when the component mounts
-    axios.get('https://api.ipify.org?format=json')
-      .then(response => {
-        const userIp = response.data.ip;
-        setIpAddress(userIp);
-      })
-      .catch(error => {
-        console.error('Error fetching IP address:', error);
-      });
-  }, []);
-
-  const handleSearch = (ip) => {
-    setIpAddress(ip);
-  }
-
   return (
-    <>
-      <Header onSearch={handleSearch} />
-      <Location ipAddress={ipAddress} />
-    </>
+    <Router>
+   <Routes>
+      <Route element={<HomeApp/>} exact path="/"  />
+      <Route exact path="/Home" element={<HomeApp/>} />
+      <Route exact path="/About_us" element={<AboutApp/>} />
+      <Route exact path="/Lawyer_directory" element={<LawyerDirectoryApp/>} />
+      <Route exact path="/result" element={<Result/>} />
+      <Route exact path="/Detail_App" element={<DeatailApp/>} />
+      <Route exact path="/review" element={<ReviewApp/>} />
+</Routes>
+    </Router>
   );
 }
 
